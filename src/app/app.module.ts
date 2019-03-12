98dc1bc8caaeb54331fd87745ae8cdf39007a168
Ivan Gil <igil.dev@gmail.com>
2019-03-12 08:32:16 -0400

    components defined, home shell implemeted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { StudentComponent } from './student/student.component';
+import { ItemComponent } from './list/item/item.component';
+import { DetailComponent } from './list/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    ListComponent,
+    StudentComponent,
+    ItemComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
