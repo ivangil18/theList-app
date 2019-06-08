0ff874e178de62d78b571085e6fb3c64c1554d21
Ivan Gil <igil.dev@gmail.com>
2019-06-08 08:17:44 -0400

    files updated

--- src/app/list/item/item.component.ts
+++ src/app/list/item/item.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { StudentDataService } from 'src/app/shared/student-data.service';
 import { Student } from 'src/app/shared/student.model';
-import { Subscription } from 'rxjs';
 import {
   trigger,
   style,
@@ -15,61 +14,15 @@ import {
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.scss'],
-  animations: [
-    trigger('listAnimation', [
-      transition('* => *', [
-        query(':enter', style({ opacity: 0 }), { optional: true }),
-
-        query(
-          ':enter',
-          stagger('300ms', [
-            animate(
-              '1s ease-in',
-              keyframes([
-                style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
-                style({
-                  opacity: 0.5,
-                  transform: 'translateY(35px)',
-                  offset: 0.3
-                }),
-                style({ opacity: 1, transform: 'translateY(0)', offset: 1.0 })
-              ])
-            )
-          ]),
-          { optional: true }
-        )
-      ])
-    ])
-  ]
+  styleUrls: ['./item.component.scss']
 })
+export class ItemComponent implements OnInit {
 
-export class ItemComponent implements OnInit, OnDestroy {
-  backColor: string;
-  studentsSub: Subscription;
-  students: Student[] = [];
+  @Input() student: Student;
 
   constructor(private studentDataService: StudentDataService) {}
 
-  ngOnInit() {
-    this.studentDataService.getStudents();
-    this.studentsSub = this.studentDataService
-      .getStudentsChanges()
-      .subscribe(studentsD => {
-        this.students = [...studentsD];
-        console.log(this.students);
-      });
-  }
-
-  ngOnDestroy() {
-    this.studentsSub.unsubscribe();
-  }
-
-  getinitials(name: string, lastName: String) {
-    const nameInitial = name.charAt(0);
-    const lastNameInitial = lastName.charAt(0);
-    return nameInitial + lastNameInitial;
-  }
+  ngOnInit() {}
 
   onShowDetails(index) {}
 
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,19 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { StudentDataService } from '../shared/student-data.service';
+import { Student } from '../shared/student.model';
+import { Subscription } from 'rxjs';
+import {
+  trigger,
+  style,
+  transition,
+  animate,
+  keyframes,
+  query,
+  stagger
+} from '@angular/animations';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), { optional: true }),
+
+        query(
+          ':enter',
+          stagger('300ms', [
+            animate(
+              '1s ease-in',
+              keyframes([
+                style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
+                style({
+                  opacity: 0.5,
+                  transform: 'translateY(35px)',
+                  offset: 0.3
+                }),
+                style({ opacity: 1, transform: 'translateY(0)', offset: 1.0 })
+              ])
+            )
+          ]),
+          { optional: true }
+        )
+      ])
+    ])
+  ]
 })
-export class ListComponent implements OnInit {
-  show = false;
+export class ListComponent implements OnInit, OnDestroy {
+  studentsSub: Subscription;
+  students: Student[] = [];
 
-  constructor() {}
+  constructor(private studentDataService: StudentDataService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.studentDataService.getStudents();
+    this.studentsSub = this.studentDataService
+      .getStudentsChanges()
+      .subscribe(studentsD => {
+        this.students = [...studentsD];
+        console.log(this.students);
+      });
+  }
 
-  showDatails(value) {
-    this.show = value;
-    console.log(value);
+  ngOnDestroy() {
+    this.studentsSub.unsubscribe();
+  }
+
+  onShowDetails(index) {}
+
+  onPresent(id) {
+    this.studentDataService.updateStudentPresent(id);
+  }
+
+  onAbsent(id) {
+    this.studentDataService.updateStudentAbsence(id);
   }
 }
